3e8f8339cd6ad3ea8be61e1f016e0dc8bc412eec
RaeleneV <raelenevdookkoo@gmail.com>
2025-11-01 10:35:15 +0200

    Add project files.

---
+++ Controllers/MaintainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeStoresApp.Controllers
+{
+    public class MaintainController : Controller
+    {
+        // GET: Maintain
+        public ActionResult Maintain()
+        {
+            return View();
+        }
+    }
+}
